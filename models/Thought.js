249253ad7c44cadac8e05a6ae790599f9beaef7c
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-11-13 19:32:49 -0600

    added in the reaction schema and adjusted the models and created the index.js for both the models and the routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,37 +1,67 @@
-const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+// Set up the ReactionSchema first so that the ThoughtSchema can ref it
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            // must be between 1-280 chars
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal)
-        },
-        username: { // the user that created this thought 
-            type: String,
-            required: true,
-        },
-        reactions: {
-            // array of nested documents created with the reactionsSchema
-        }
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        }, 
-        id: false,
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      // the user that created this thought
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 ThoughtSchema.virtual("reactionCount").get(function () {
-    return this.reactions.reduce((total, reactions) => total + reactions.length +1, 0);
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", ThoughtSchema);
--- models/User.js
+++ models/User.js
@@ -1,38 +1,46 @@
 // import the schema constructor and the model function from mongoose
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true, 
-            // validate?
-        },
-        thoughts: {
-            // Arr of _id values refs the Thought model
-        },
-        friends: {
-            // Arr of _id values refs the User model (self-reference)
-        },
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 UserSchema.virtual("friendCount").get(function () {
-    return this.friends.reduce((total, friends) => total + this.friends.length +1, 0);
+  return this.friends.reduce(
+    (total, user) => total + user.friends.length + 1,
+    0
+  );
 });
 
 const User = model("User", UserSchema);
